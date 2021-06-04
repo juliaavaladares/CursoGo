28437ba322fdb20ad792e35045748a92250cea10
Julia Valadares <julia.valadares@luizalabs.com>
2021-06-04 10:16:06 -0300

    Finaliza a aulas sobre pacotes

--- aula01_Pacotes/main.go
+++ aula01_Pacotes/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main()  {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	error := checkmail.ValidateFormat("devbook@gmail.com")
+	fmt.Println(error)
+
 }
